cb8deac4866037fb3e4633eccab3f6db543cdcce
Jayesh Thar <jayeshthar1409@gmail.com>
2026-06-13 16:33:43 +0530

    add: advance pattern printing questions

--- Pattern.java
+++ Pattern.java
@@ -84,21 +84,21 @@ public class Pattern {
             }
             System.out.println();
         }
-        
+
         System.out.println();
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(j+ " ");
+                System.out.print(j + " ");
             }
             System.out.println();
         }
-        
+
         System.out.println();
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(i+ " ");
+                System.out.print(i + " ");
             }
             System.out.println();
         }
@@ -113,7 +113,7 @@ public class Pattern {
                 System.out.println();
             } else {
                 for (int j = 1; j <= i; j++) {
-                    System.out.print(j+" ");
+                    System.out.print(j + " ");
                 }
                 System.out.println();
             }
@@ -129,5 +129,124 @@ public class Pattern {
         }
 
         System.out.println();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n + 1 - i; j++) {
+                System.out.print("+ ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // exceptional case:
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j <= n + 1 - i; j++) {
+                System.out.print("+ ");
+            }
+            System.out.println();
+        }
+        // NOTE: same we do by if() to skip one line alternative....
+
+        System.out.println();
+
+        int a = n;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= a; j++) {
+                System.out.print("@ ");
+            }
+            a--;
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // Hollow square/rectangle
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // plus star
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == ((n / 2) + 1) || j == ((n / 2) + 1)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // cross star
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j || j == n + 1 - i) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // Floyd's triangle
+        int count = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                // System.out.print(count + " ");
+                // count++;
+                System.out.print(count++ + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // binary alternative triangle
+        int bin = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (bin == 1) {
+                    System.out.print(bin);
+                    bin = 0;
+                } else {
+                    System.out.print(bin);
+                    bin = 1;
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // binary triangle
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if((i+j)%2==0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        
     }
 }
