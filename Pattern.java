146df117fd76afdaae6a43737847551c64385a77
Jayesh Thar <jayeshthar1409@gmail.com>
2026-06-13 12:54:35 +0530

    add: pattern printing: square(interger & character), triangle and it's right angle triangle shapes

---
+++ Pattern.java
@@ -0,0 +1,133 @@
+
+public class Pattern {
+    public static void main(String[] args) {
+
+        // nestedLoop: loop inside loop...
+        for (int i = 1; i <= 4; i++) { // ROW's - how many line
+            for (int j = 1; j <= 5; j++) { // COLUMN's - how many star to print in that one row's
+                System.out.print(" * ");
+            }
+            System.out.println(); // next line
+        }
+
+        System.out.println();
+
+        for (int i = 1; i <= 4; i++) { // ROW's - how many line
+            for (int j = 1; j <= 5; j++) { // COLUMN's - how many star to print in that one row's
+                System.out.print(j + " ");
+            }
+            System.out.println(); // next line
+        }
+
+        System.out.println();
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            int nums = 65;
+            for (int j = 1; j <= n; j++) {
+                System.out.print((char) nums + " ");
+                nums++;
+
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // optimized:
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print((char) (j + 64) + " "); // replace 96 for 'a' - 97
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print((char) (i + 64) + " "); // replace 96 for 'a' - 97
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                for (int j = 1; j <= n; j++) {
+                    System.out.print((char) (i + 64) + " ");
+                }
+                System.out.println();
+            } else {
+                for (int j = 1; j <= n; j++) {
+                    System.out.print((char) (i + 96) + " ");
+                }
+                System.out.println();
+            }
+        }
+
+        System.out.println();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+        System.out.println();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j+ " ");
+            }
+            System.out.println();
+        }
+        
+        System.out.println();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i+ " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print((char) (j + 64) + " ");
+                }
+                System.out.println();
+            } else {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(j+" ");
+                }
+                System.out.println();
+            }
+        }
+
+        System.out.println();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j >= i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+    }
+}
